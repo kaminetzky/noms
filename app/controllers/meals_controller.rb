aa1b3681aa1d7c82c28a34146970820d63be3efd
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-06-10 23:18:45 -0400

    Add temporary meal controller methods

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,2 +1,50 @@
 class MealsController < ApplicationController
+  def index
+    @meals = Meal.all
+  end
+
+  def show
+    @meal = Meal.find(params[:id])
+  end
+
+  def new
+    @meal = Meal.new
+  end
+
+  def create
+    @meal = Meal.new(meal_params)
+
+    if @meal.save
+      redirect_to @meal
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @meal = Meal.find(params[:id])
+  end
+
+  def update
+    @meal = Meal.find(params[:id])
+
+    if @meal.update(meal_params)
+      redirect_to @meal
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @meal = Meal.find(params[:id])
+    @meal.destroy
+
+    redirect_to meals_path, status: :see_other
+  end
+
+  private
+    def meal_params
+      params.require(:meal).permit(:description, :servings,
+                                   :consumption_datetime, :food)
+    end
 end
