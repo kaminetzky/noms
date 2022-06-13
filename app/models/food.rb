235a5d3828a746904dad5c8439db5e9354636ca7
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-06-12 21:50:27 -0400

    Verify presence of serving info

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,8 +2,8 @@ class Food < ApplicationRecord
   has_many :meals
 
   validates :name, presence: true
-  validates :serving_size, comparison: { greater_than_or_equal_to: 0 },
-    allow_nil: true
+  validates :serving_size, comparison: { greater_than_or_equal_to: 0 }
+  validates :serving_unit, presence: true
   validates :calories, comparison: { greater_than_or_equal_to: 0 }
   validates :protein, comparison: { greater_than_or_equal_to: 0 }
 end
