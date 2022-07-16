d341057279a474d8412ea2ac8c23e991decd22cb
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-07-16 19:36:24 -0400

    Begin dashboard makeover

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,10 +1,10 @@
 class DashboardsController < ApplicationController
   def show
-
     @meal_count = Meal.count
     @food_count = Food.count
 
     @meals_today = Meal.today
     @meal_last = Meal.last
+    @food_last = Food.last
   end
 end
