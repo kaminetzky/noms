8d57340a961bfd7ce177c990d555372590275dba
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-08-20 02:57:54 -0400

    Fully implement previous arrow (not completely tested yet)

--- app/helpers/meals_helper.rb
+++ app/helpers/meals_helper.rb
@@ -6,7 +6,7 @@ module MealsHelper
     ending = meals_user.maximum('consumed_on') - 6.hours
 
     on_same_year = ending.year == starting.year
-    on_same_month = ending.year == starting.year && ending.month == starting.month
+    on_same_month = on_same_year && ending.month == starting.month
 
     last_year_page = ending.year - starting.year + 1
     last_month_page = on_same_year ? ending.month - starting.month + 1 : ending.month
@@ -18,10 +18,43 @@ module MealsHelper
   end
 
   def prev_day_params(calendar)
-    # To be implemented
-    { year_page: calendar[:year].page,
-      month_page: calendar[:month].page,
-      day_page: [calendar[:day].page - 1, 1].max }
+    prev_day_page = calendar[:day].page - 1
+    prev_month_page = calendar[:month].page
+    prev_year_page = calendar[:year].page
+
+    if prev_day_page.zero?
+      prev_month = calendar[:month].from.month - 1
+      prev_year = calendar[:year].from.year
+
+      if prev_month.zero?
+        prev_month = 12
+        prev_year -= 1
+      end
+
+      on_same_year = prev_year == calendar[:year].starting.year
+      on_same_month = on_same_year && prev_month == calendar[:year].starting.month
+
+      month_offset = on_same_year ? calendar[:year].starting.month - 1 : 0
+      day_offset = on_same_month ? calendar[:year].starting.day - 1 : 0
+
+      prev_day_page = Time.days_in_month(prev_month, prev_year) - day_offset
+      prev_month_page -= 1
+
+      if prev_month_page.zero?
+        prev_month_page = 12 - month_offset
+        prev_year_page -= 1
+
+        if prev_year_page.zero?
+          prev_year_page = 1
+          prev_month_page = 1
+          prev_day_page = 1
+        end
+      end
+    end
+
+    { year_page: prev_year_page,
+      month_page: prev_month_page,
+      day_page: prev_day_page }
   end
 
   def next_day_params(calendar)
@@ -32,17 +65,17 @@ module MealsHelper
     if next_day_page > calendar[:day].pages
       next_day_page = 1
       next_month_page += 1
-    end
 
-    if next_month_page > calendar[:month].pages
-      next_month_page = 1
-      next_year_page += 1
-    end
+      if next_month_page > calendar[:month].pages
+        next_month_page = 1
+        next_year_page += 1
 
-    if next_year_page > calendar[:year].pages
-      next_day_page = calendar[:day].page
-      next_month_page = calendar[:month].page
-      next_year_page = calendar[:year].page
+        if next_year_page > calendar[:year].pages
+          next_day_page = calendar[:day].page
+          next_month_page = calendar[:month].page
+          next_year_page = calendar[:year].page
+        end
+      end
     end
 
     { year_page: next_year_page,
