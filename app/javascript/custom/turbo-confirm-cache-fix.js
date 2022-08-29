1a349e847ee3f9b5bf9ae4bd9307e1e92a0d6e8f
Alex Kaminetzky <akaminetzkyp@gmail.com>
2022-08-29 01:11:53 -0400

    Remove function call in turbo fix js

--- app/javascript/custom/turbo-confirm-cache-fix.js
+++ app/javascript/custom/turbo-confirm-cache-fix.js
@@ -1,19 +1,17 @@
-;(function () {
-  var ua = window.navigator.userAgent
-  var isIpad = ua.indexOf('(iPad') > -1
-  var isIphone = ua.indexOf('(iPhone') > -1 || ua.indexOf('(iPod') > -1
-  var isChrome = ua.indexOf('Chrome/') > -1 || ua.indexOf('CriOS/') > -1
-  var isSafari = !isChrome && ua.indexOf('Safari/') > -1
-  var isIosSafari = (isIpad || isIphone) && isSafari
+var ua = window.navigator.userAgent
+var isIpad = ua.indexOf('(iPad') > -1
+var isIphone = ua.indexOf('(iPhone') > -1 || ua.indexOf('(iPod') > -1
+var isChrome = ua.indexOf('Chrome/') > -1 || ua.indexOf('CriOS/') > -1
+var isSafari = !isChrome && ua.indexOf('Safari/') > -1
+var isIosSafari = (isIpad || isIphone) && isSafari
 
-  if (isIosSafari) {
-    var popped
-    window.addEventListener('popstate', function () { popped = true })
-    
-    document.addEventListener('turbo:before-render', function (event) {
-      if (popped && event.detail.newBody.querySelector('[data-turbo-confirm]')) {
-        window.location.reload()
-      }
-    })
-  }
-})()
+if (isIosSafari) {
+  var popped
+  window.addEventListener('popstate', function () { popped = true })
+  
+  document.addEventListener('turbo:before-render', function (event) {
+    if (popped && event.detail.newBody.querySelector('[data-turbo-confirm]')) {
+      window.location.reload()
+    }
+  })
+}
